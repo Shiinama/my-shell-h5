9f91c8f0ffda0c052b9b59dc077fbfe85c3cd7dc
weishunyu <2072738187@qq.com>
2023-05-10 20:45:24 +0800

    feat: telegram推送服务

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { useEffect, useRef } from 'react'
 import TelegramLoginButton from 'react-telegram-login'
 
 import './App.css'
 
 function App() {
   const handleTelegramResponse = response => {
-    console.log(response)
+    window.ReactNativeWebView.postMessage(JSON.stringify(response))
   }
   return (
     <>
-      <button
-        onClick={() => {
-          window.ReactNativeWebView.postMessage('1231231321')
-          console.log(111)
-        }}
-      >
-        213123
-      </button>
       <TelegramLoginButton dataOnauth={handleTelegramResponse} botName="Aishell_bot" />,
     </>
   )
